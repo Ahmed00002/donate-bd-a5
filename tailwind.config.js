417723e41909cb70337770f8672b41cc36532e88
Layek Ahmed Numan <reduanahmed773@gmail.com>
2024-09-23 14:45:20 -0300

    added donation tab and empty history tab

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,12 @@ module.exports = {
       fontFamily: {
         inter: ["Inter", "sans-serif"],
         poppins: ["Poppins", "sans-serif"],
+        lexend: ["Lexend", "sans-serif"],
+      },
+      colors: {
+        paraColor: "rgba(17, 17, 17, 0.7)",
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
